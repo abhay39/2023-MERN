a1ceaa2064ff91f05da4ab1946a369fac56e43b9
abhay39 <abhayguptaak39@gmail.com>
2023-11-11 09:55:50 +0530

    chapter 1

---
+++ fileReading.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+
+
+// reading file from local files
+// console.log(performance.now());
+// const txt=fs.readFileSync("abhay.txt",'utf-8');
+// console.log(txt);
+
+// fs.readFile("abhay.txt",'utf-8',(err,text)=>{
+//     console.log(text)
+// });
+// console.log(performance.now());
+
+
